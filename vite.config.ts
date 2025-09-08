540972a0091ee5d445a62251af3dec44cf3fa0a2
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-09-08 19:27:13 +0900

    chore: add ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { cloudflare } from '@cloudflare/vite-plugin'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { reactRouter } from '@react-router/dev/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
+    react(),
     cloudflare({ viteEnvironment: { name: 'ssr' } }),
     reactRouter(),
     tsconfigPaths(),
