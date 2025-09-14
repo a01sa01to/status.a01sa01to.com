fe2533b492810688456e772806a51cb2d0401d0c
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-09-15 01:12:55 +0900

    fix worker types

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,14 @@
 import myConfig from '@a01sa01to/eslint-config'
 
-export default [...myConfig, { ignores: ['.yarn/*', '.pnp*', '**/build/*'] }]
+export default [
+  ...myConfig,
+  {
+    ignores: [
+      '.yarn/*',
+      '.pnp*',
+      '**/build/*',
+      '.react-router/*',
+      'worker-configuration.d.ts',
+    ],
+  },
+]
